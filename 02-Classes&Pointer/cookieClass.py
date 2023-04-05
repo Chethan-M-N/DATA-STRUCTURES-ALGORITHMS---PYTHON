539c074bea9c6b90cb2ba6b0d697fddc2dd13982
Chethan-Medahalli NARAYANAMURTHY (EXT) <chethan-medahalli.narayanamurthy.ext@valeo.com>
2023-04-05 12:12:59 +0200

    Added Class & Pointer folder

---
+++ 02-Classes&Pointer/cookieClass.py
@@ -0,0 +1,26 @@
+# Class : is user defined data type/structure
+# self keyword : It's a method that is part of a class
+# __init__ : Initializing the constructur
+
+class Cookie:
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def set_color(self, color):
+        self.color = color
+
+
+cookie_one = Cookie('green')
+cookie_two = Cookie('blue')
+
+print('Cookie one is', cookie_one.get_color())
+print('Cookie two is', cookie_two.get_color())
+
+cookie_one.set_color('yellow')
+
+print('\nCookie one is now', cookie_one.get_color())
+print('Cookie two is still', cookie_two.get_color())
+
